eccf8c502ae33a4b160701e1d551d209705b3d69
iruyj <jeon1310@gmail.com>
2021-08-17 23:43:01 +0900

    CalenderFragment.kt add event dot

---
+++ app/src/main/java/Decorator/DotDecorator.kt
@@ -0,0 +1,28 @@
+package Decorator
+
+import android.graphics.Color
+import android.util.Log
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+
+
+
+class DotDecorator(private val color: String, dates: Collection<CalendarDay>?) : DayViewDecorator {
+    private val dates: HashSet<CalendarDay> = HashSet(dates)
+    //private val drawble : Drawable? = ContextCompat.getDrawable(context,R.drawable.calendar_addspan1)
+
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        Log.d(CalendarDay.today().toString(), "decorate: 실행됨 dot1")
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(DotSpan(5F, Color.parseColor(color)))
+        //view.addSpan(drawble!!)
+
+        Log.d(dates.toString(), "decorate: 실행됨 dot")
+    }
+
+}
--- app/src/main/java/kr/hs/emirim/w2015/stac_prr/Fragment/CalenderFragment.kt
+++ app/src/main/java/kr/hs/emirim/w2015/stac_prr/Fragment/CalenderFragment.kt
@@ -1,5 +1,6 @@
 package kr.hs.emirim.w2015.stac_prr.Fragment
 
+import Decorator.DotDecorator
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Rect
@@ -28,11 +29,12 @@ import java.util.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class CalenderFragment : Fragment(),View.OnClickListener {
+class CalenderFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentCalenderBinding? = null//<layout></layout>으로 감싼 것만 바인딩가능
     private val binding get() = _binding!!
     lateinit var adapter: PlanAdapter
     var model = ItemModel()
+    val dotPlanDay = mutableListOf<CalendarDay>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,42 +47,12 @@ class CalenderFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // 캘린더 부분 변수선언
-        var startTimeCalendar = Calendar.getInstance()
-        var endTimeCalendar = Calendar.getInstance()
-        materialCalendar.topbarVisible = true
-
-        val currentYear = startTimeCalendar.get(Calendar.YEAR)
-        val currentMonth = startTimeCalendar.get(Calendar.MONTH)
-        val currentDate = startTimeCalendar.get(Calendar.DATE)
-        
-        endTimeCalendar.set(Calendar.MONTH, currentMonth+3)
-
-        // 달력 처음 설정
-        binding.materialCalendar.state().edit()
-            .setMinimumDate(CalendarDay.from(2017, 8, 1))
-            .setMaximumDate(CalendarDay.from(currentYear+3, currentMonth, endTimeCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)))
-            .setCalendarDisplayMode(CalendarMode.MONTHS)
-            .commit()
-
-        // 헤더 레이블 수정
-        val date = Calendar.getInstance()
-
-        //글자 커스텀
-        binding.materialCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
-        binding.materialCalendar.setWeekDayTextAppearance(R.style.CalendarWidgetWeek)
-        binding.materialCalendar.setDateTextAppearance(R.style.CalendarWidgetDate)
-
-        // 부가적 커스텀
-        binding.materialCalendar.setTileHeightDp(55) // 타이틀 높이
-        binding.materialCalendar.setSelectedDate(CalendarDay.today())  // 오늘 선택
-
-        val sundayDecorator = SundayDecorator()     // 일요일 빨간색
-        val saturdayDecorator = SaturdayDecorator() // 토요일 파란색
-        val todayDecorator = TodayDecorator(requireContext())   //오늘 배경설정
-
-        //데코레이터 추가
-        binding.materialCalendar.addDecorators(sundayDecorator, saturdayDecorator, todayDecorator)
+        // dot 추가할 리스트 넣기 - month는 인덱스가 0이라서 1월 -> 0월로 적어야한다
+        dotPlanDay.add(CalendarDay.from(2021, 8 - 1, 18))
+        dotPlanDay.add(CalendarDay.from(2021, 8 - 1, 27))
+        dotPlanDay.add(CalendarDay.from(2021, 8 - 1, 10))
+        calendarOption()
+        addDecorator()
 
         //날짜 클릭했을때
         binding.materialCalendar.setOnDateChangedListener { widget, date, selected ->
@@ -88,23 +60,12 @@ class CalenderFragment : Fragment(),View.OnClickListener {
             val date: String = simpleDateFormat.format(date.getCalendar().getTime())
 
             binding.planTxt.text = date
-           /* Toast.makeText(
-                ApplicationProvider.getApplicationContext<Context>(),
-                date,
-                Toast.LENGTH_SHORT
-            ).show()
-            val CAIntent = Intent(
-                ApplicationProvider.getApplicationContext<Context>(),
-                ConfirmScheduleActivity::class.java
-            )
-            CAIntent.putExtra("날짜", date)
-            startActivity(CAIntent)*/
         }
 
         binding.planRecyclerview.addItemDecoration(RecyclerViewDecoration(18))
         init()  // recycleview 설정
         //plus btn listener
-        binding.planPlusBtn.setOnClickListener(){
+        binding.planPlusBtn.setOnClickListener() {
             val activity = activity as MainActivity
             activity.fragmentChange_for_adapter(AddPlanFragment())
             Log.d("plus", "plus버튼 클릭됨")
@@ -117,8 +78,9 @@ class CalenderFragment : Fragment(),View.OnClickListener {
         프래그먼트는 뷰 보다 오래 지속되기 때문에 onDestroyView()에서 binding class 인스턴스 null 값으로 변경하여 참조를 정리해주어야 합니다.*/
         _binding = null
     }
+
     // 여기부터 리사이클뷰 코드-------------------
-    fun init(){
+    fun init() {
         // 리사이클뷰 관련 변수 선언
         model.makeTestItems()
         adapter = PlanAdapter()
@@ -134,41 +96,100 @@ class CalenderFragment : Fragment(),View.OnClickListener {
         Log.d("", "onClick: 버튼이 없기때문에 없어도됨")
     }
 
+    fun calendarOption() {
+        // 캘린더 부분 변수선언
+        var startTimeCalendar = Calendar.getInstance()
+        var endTimeCalendar = Calendar.getInstance()
+        materialCalendar.topbarVisible = true
+
+        val currentYear = startTimeCalendar.get(Calendar.YEAR)
+        val currentMonth = startTimeCalendar.get(Calendar.MONTH)
+        val currentDate = startTimeCalendar.get(Calendar.DATE)
+
+        endTimeCalendar.set(Calendar.MONTH, currentMonth + 3)
+
+        // 달력 처음 설정
+        binding.materialCalendar.state().edit()
+            .setMinimumDate(CalendarDay.from(2017, 8, 1))
+            .setMaximumDate(
+                CalendarDay.from(
+                    currentYear + 3,
+                    currentMonth,
+                    endTimeCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+                )
+            )
+            .setCalendarDisplayMode(CalendarMode.MONTHS)
+            .commit()
+
+        // 헤더 레이블 수정
+        val date = Calendar.getInstance()
+
+        //글자 커스텀
+        binding.materialCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
+        binding.materialCalendar.setWeekDayTextAppearance(R.style.CalendarWidgetWeek)
+        binding.materialCalendar.setDateTextAppearance(R.style.CalendarWidgetDate)
+
+        // 부가적 커스텀
+        binding.materialCalendar.setTileHeightDp(55) // 타이틀 높이
+        binding.materialCalendar.setSelectedDate(CalendarDay.today())  // 오늘 선택
+    }
+
+    fun addDecorator() {
+        val sundayDecorator = SundayDecorator()     // 일요일 빨간색
+        val saturdayDecorator = SaturdayDecorator() // 토요일 파란색
+        val todayDecorator = TodayDecorator(requireContext())   //오늘 배경설정
+        val dotDecorator = DotDecorator("#ff0000", dotPlanDay)
+
+        //데코레이터 추가
+        binding.materialCalendar.addDecorators(
+            sundayDecorator,
+            saturdayDecorator,
+            todayDecorator,
+            dotDecorator
+        )
+
+    }
+
 }
 
 // 투데이 데코레이터
-class TodayDecorator(context: Context): DayViewDecorator {
+class TodayDecorator(context: Context) : DayViewDecorator {
     private var date = CalendarDay.today()
     val drawable = context.resources.getDrawable(R.drawable.calendar_today_back)   // 오늘날짜 배경 설정
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         return day?.equals(date)!!
     }
+
     override fun decorate(view: DayViewFacade?) {
         view?.setBackgroundDrawable(drawable)
     }
 }
+
 // 일요일 빨갛게
-class SundayDecorator:DayViewDecorator {
+class SundayDecorator : DayViewDecorator {
     private val calendar = Calendar.getInstance()
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         day?.copyTo(calendar)
         val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
         return weekDay == Calendar.SUNDAY
     }
+
     override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(object: ForegroundColorSpan(Color.parseColor("#EA7159")){})
+        view?.addSpan(object : ForegroundColorSpan(Color.parseColor("#EA7159")) {})
     }
 }
+
 // 토요일 파랗게
-class SaturdayDecorator:DayViewDecorator {
+class SaturdayDecorator : DayViewDecorator {
     private val calendar = Calendar.getInstance()
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         day?.copyTo(calendar)
         val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
         return weekDay == Calendar.SATURDAY
     }
+
     override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(object: ForegroundColorSpan(Color.parseColor("#7A8FDE")){})
+        view?.addSpan(object : ForegroundColorSpan(Color.parseColor("#7A8FDE")) {})
     }
 }
 
