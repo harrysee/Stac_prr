c128368dcd074e322249d95f2ea06b3b93d60085
iruyj <jeon1310@gmail.com>
2021-08-28 23:28:30 +0900

    NewPlantFragment.kt 사진 업로드

--- app/src/main/java/kr/hs/emirim/w2015/stac_prr/Fragment/NewPlantFragment.kt
+++ app/src/main/java/kr/hs/emirim/w2015/stac_prr/Fragment/NewPlantFragment.kt
@@ -1,6 +1,9 @@
 package kr.hs.emirim.w2015.stac_prr.Fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +14,13 @@ import kr.hs.emirim.w2015.stac_prr.CustomDialog
 import kr.hs.emirim.w2015.stac_prr.MainActivity
 import kr.hs.emirim.w2015.stac_prr.R
 
+
 class NewPlantFragment : Fragment(){
     var db : FirebaseFirestore? = null
+    //val android_id = Settings.Secure.getString(context?.contentResolver, Settings.Secure.ANDROID_ID)
+    val FROM_ALBUM = 200
+    lateinit var photoURI : Uri
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +39,7 @@ class NewPlantFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
         val activity = activity as MainActivity
         db = FirebaseFirestore.getInstance()
-        
+
         // 이미지 화살표 눌렀을때
         img_btn_backhome.setOnClickListener(){
             val dir = CustomDialog(requireContext())
@@ -48,5 +56,31 @@ class NewPlantFragment : Fragment(){
             activity.fragmentChange_for_adapter(HomeFragment())
         }
 
+        newplant_upload_btn.setOnClickListener{
+            //앨범 열기
+            val intent = Intent(Intent.ACTION_PICK)
+
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            intent.type = "image/*"
+            //intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            startActivityForResult(intent, FROM_ALBUM)
+
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data?.data != null) {
+            try {
+                photoURI = data.data!!
+                val bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, photoURI)
+                newplant_upload_btn.setImageBitmap(bitmap)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
 }
