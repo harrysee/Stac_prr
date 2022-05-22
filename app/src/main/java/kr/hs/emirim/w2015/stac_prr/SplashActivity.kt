d66ff2d1f8f232f018a9e8c7a214ee0ccb61096a
Jyuri <jeon1310@gmail.com>
2022-05-22 21:49:59 +0900

    splash logo gif 변경

--- app/src/main/java/kr/hs/emirim/w2015/stac_prr/SplashActivity.kt
+++ app/src/main/java/kr/hs/emirim/w2015/stac_prr/SplashActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import androidx.core.os.postDelayed
+import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val splashlogo = findViewById<ImageView>(R.id.imageView)
+        Glide.with(this).load(R.raw.prr_logo).into(splashlogo)
 
         val handler = Handler()
         handler.postDelayed({
             val intent = Intent(getApplication(), MainActivity::class.java)
             startActivity(intent)
             finish()
-        },1000)
+        },3000)
     }
 }
